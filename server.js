326935b76e05c839bf7f91d7b8f2f3b6150cd753
sami9994 <samiomar994@gmail.com>
2022-10-21 09:46:09 +0300

    add home route

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ require('dotenv').config()
 app.use(cors())
 
 app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('ist working')
+})
 app.post('/', async (req, res) => {
   const { mainUrl } = req.body
   const linkIsHere = await Link.findOne({ mainUrl })
